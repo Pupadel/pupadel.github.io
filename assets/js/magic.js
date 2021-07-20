55596c4cf990b35cb81ebeb4b5d11f8fbcfc28f1
Pupadel <oleg.2018.goncharov@gmail.com>
2021-07-20 11:11:18 +0300

    блять гит

---
+++ assets/js/magic.js
@@ -0,0 +1,6 @@
+$(document).ready(()=> {
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    if (isFirefox) {
+        $('body').css('background', '#0c0c0c')
+    }
+})
---
+++ assets/js/preloader.js
@@ -0,0 +1,6 @@
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+    $spinner   = $preloader.find('.spin');
+    $spinner.delay(100).fadeOut('slow');
+    $preloader.delay(450).fadeOut('slow');
+});
